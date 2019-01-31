a1eed10f52c4d2d105611c05dd765699ca7511c5
Justin Gitterman <justingitterman@gmail.com>
2019-01-31 17:16:28 -0500

    created the necessary files, and created the database

---
+++ seeds.sql
@@ -0,0 +1,51 @@
+use bamazon;
+
+INSERT INTO products
+  (product_name, department_name, price, stock_quantity)
+VALUES
+  ('gameboy', 'electronics', 50, 25);
+
+INSERT INTO products
+  (product_name, department_name, price, stock_quantity)
+VALUES
+  ('ps4', 'electronics', 200, 25);
+
+INSERT INTO products
+  (product_name, department_name, price, stock_quantity)
+VALUES
+  ('iphoneX', 'electronics', 500, 10);
+
+INSERT INTO products
+  (product_name, department_name, price, stock_quantity)
+VALUES
+  ('ipad', 'electronics', 250, 15);
+
+INSERT INTO products
+  (product_name, department_name, price, stock_quantity)
+VALUES
+  ('xbox', 'electronics', 200, 20);
+
+INSERT INTO products
+  (product_name, department_name, price, stock_quantity)
+VALUES
+  ('lamp', 'homegoods', 20, 25);
+
+INSERT INTO products
+  (product_name, department_name, price, stock_quantity)
+VALUES
+  ('couch', 'homegoods', 150, 5);
+
+INSERT INTO products
+  (product_name, department_name, price, stock_quantity)
+VALUES
+  ('television', 'electronics', 300, 15);
+
+INSERT INTO products
+  (product_name, department_name, price, stock_quantity)
+VALUES
+  ('toilet paper', 'homegoods', 5, 250);
+
+INSERT INTO products
+  (product_name, department_name, price, stock_quantity)
+VALUES
+  ('table', 'homegoods', 50, 25);
